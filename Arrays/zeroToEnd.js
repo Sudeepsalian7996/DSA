dac40487fd5bc0fa7a6621e0459859cb736e155f
Sudeepsalian7996 <sudeep7996>
2023-05-02 08:50:24 +0530

    moving zero's to the end

---
+++ Arrays/zeroToEnd.js
@@ -0,0 +1,13 @@
+nums = [0,2,4,0,6]
+  let j=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==0) j++
+        else [nums[i-j],nums[i]]=[nums[i],nums[i-j]]
+    }
+    
+   console.log(nums)
+   
+//   // [
+//   1, 4, 3, 12, 15,
+//   0, 0, 0,  0,  0
+// ]
